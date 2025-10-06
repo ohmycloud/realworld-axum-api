d27d8ad226b728fa11d0052edaa86b8cf52c62ce
ohmycloud <ohmycloudy@gmail.com>
2025-10-06 10:29:59 +0800

    add password update to user repository

--- src/repositories/user_repository.rs
+++ src/repositories/user_repository.rs
@@ -119,4 +119,24 @@ impl UserRespositoryTrait for UserRepository {
 
         Ok(user)
     }
+
+    async fn update_password(
+        &self,
+        user_id: Uuid,
+        new_password_hash: &str,
+    ) -> Result<(), sqlx::Error> {
+        sqlx::query(
+            r#"
+            UPDATE users
+            SET password_hash = $2
+            WHERE id = $1
+            "#,
+        )
+        .bind(user_id)
+        .bind(new_password_hash)
+        .execute(&self.db_pool)
+        .await?;
+
+        Ok(())
+    }
 }
